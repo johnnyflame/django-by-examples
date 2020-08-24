37f9691ccfe01c25a462184e9f3cfde14ca85272
johnnyflame <johnnyflamelee@gmail.com>
2020-08-24 23:04:33 +1200

    added search feature to the blog

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post, Comment
 from django.views.generic import ListView
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 from django.core.mail import send_mail
 from taggit.models import Tag
 from django.db.models import Count
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+
 
 # Create your views here.
 def post_share(request, post_id):
@@ -105,3 +107,28 @@ def post_detail(request, year, month, day, post):
             "similar_posts": similar_posts,
         },
     )
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+    if "query" in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data["query"]
+            search_vector = SearchVector("title", weight="A") + SearchVector(
+                "body", weight="B"
+            )
+            search_query = SearchQuery(query)
+            results = (
+                Post.published.annotate(rank=SearchRank(search_vector, search_query))
+                .filter(rank__gte=0.1)
+                .order_by("-rank")
+            )
+    return render(
+        request,
+        "blog/post/search.html",
+        {"form": form, "query": query, "results": results},
+    )
+
